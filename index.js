620e0c74c9aa318cc6457e6d7862d9a508989bbd
Kate Hudson <k88hudson@gmail.com>
2015-10-07 21:16:38 -0400

    added files

---
+++ index.js
@@ -0,0 +1,50 @@
+const child_process = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+const colors = require('colors');
+
+const LOG_PREFIX = '** karma-template:';
+const SRC_DIR = path.join(__dirname, 'src');
+const DEPENDENCIES = [
+  'karma',
+  'karma-babel-preprocessor',
+  'karma-chrome-launcher',
+  'karma-firefox-launcher',
+  'karma-mocha',
+  'karma-mocha-reporter',
+  'karma-sourcemap-loader',
+  'karma-webpack'
+];
+
+const argv = require('minimist')(process.argv.slice(2));
+
+function log(text, error) {
+  console.log(`${LOG_PREFIX} ${text}`[error ? 'red' : 'green']);
+}
+
+function installDependencies() {
+  const saveCommand = argv.save !== false ? '--save-dev' : '';
+  log('Installing dependencies...');
+  child_process.execSync(`npm install ${DEPENDENCIES.join(' ')} ${saveCommand}`, {
+    stdio: [0, process.stdout, process.stderr]
+  });
+}
+
+function copyFiles() {
+  log('Copying files...');
+  try {
+    fs.copySync(SRC_DIR, '.', {clobber: argv.force});
+  } catch (err) {
+    log(`Oops, could not copy files!
+
+   Do you already have a ./tests directory or a karma.conf.js file?
+   This tool won't overwrite them unless you use --force`, 'error');
+    console.log(err.stack.red);
+    process.exit(1);
+  }
+
+  log('Success! Files copied.');
+}
+
+if (argv.install !== false) installDependencies();
+copyFiles();
---
+++ src/karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function (config) {
+  config.set({
+    singleRun: true,
+    browsers: ['Firefox'],
+    frameworks: ['mocha'],
+    reporters: ['mocha'],
+    files: [
+      'tests/**/*.js'
+    ],
+    preprocessors: {
+     'tests/**/*.js': ['babel', 'webpack', 'sourcemap']
+    },
+    webpack: {
+      devtool: 'inline-source-map'
+    }
+  });
+};
